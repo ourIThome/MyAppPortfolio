3b9d182239f4207806c861266bad4d58f95246a1
Stephen Day <stephen.day@ourIThome.com>
2015-07-29 21:43:50 +0100

    Toast

--- app/src/main/java/com/ourithome/myappportfolio/MainActivity.java
+++ app/src/main/java/com/ourithome/myappportfolio/MainActivity.java
@@ -1,9 +1,15 @@
 package com.ourithome.myappportfolio;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -34,4 +40,30 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void showToast(View view) {
+
+        LayoutInflater inflater = getLayoutInflater();
+
+        //This breaks my code!!!
+        //View layout = inflater.inflate(R.layout.activity_main,
+                //        (ViewGroup) findViewById(R.id.toast_Layout_root));
+
+        //show toast message because it was clicked
+        Button button = (Button) view;
+
+        //get text from button
+        String buttonText = (String) button.getText();
+        Context context = getApplicationContext();
+
+        //make toast and show it
+        Toast toast = Toast.makeText(context, buttonText, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER|Gravity.BOTTOM, 0, 0);
+        toast.setDuration(Toast.LENGTH_LONG);
+
+        //not using this as the layout gets broken earlier in the code.
+        //toast.setView(layout);
+        toast.show();
+    }
+
 }
